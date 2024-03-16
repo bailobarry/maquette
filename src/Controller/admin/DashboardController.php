e5e4cc2f391e453932584041040d9a7bbfbc6e0e
bailobarry <barrymamadoubailo23@gmail.com>
2024-03-16 17:15:03 +0100

    Ajout des requirements dans les routes

--- src/Controller/admin/DashboardController.php
+++ src/Controller/admin/DashboardController.php
@@ -20,50 +20,5 @@
         {
             return $this -> render('authentification/register.html.twig');
         }
-
-        public function utilisateur(): Response
-        {
-            return $this-> render('admin/utilisateurs/utilisateur.html.twig');
-        }
-
-        public function diplomes(): Response
-        {
-            return $this-> render('admin/maquettes/diplomes/ajouter.html.twig');
-        }
-
-        public function parcours(): Response
-        {
-            return $this-> render('admin/maquettes/parcours/index.html.twig');
-        }
-
-        public function mcc(): Response
-        {
-            return $this-> render('admin/maquettes/modalites_controles/index.html.twig');
-        }
-
-        public function ues(): Response
-        {
-            return $this-> render('admin/maquettes/ues/index.html.twig');
-        }
-
-        public function blocs_competences(): Response
-        {
-            return $this-> render('admin/maquettes/blocs_competences/index.html.twig');
-        }
-
-        public function competence(): Response
-        {
-            return $this-> render('admin/maquettes/competences/index.html.twig');
-        }
-
-        public function blocs_connaissances(): Response
-        {
-            return $this-> render('admin/maquettes/blocs_connaissances/index.html.twig');
-        }
-
-        public function connaissance(): Response
-        {
-            return $this-> render('admin/maquettes/connaissances/index.html.twig');
-        }
     }
 ?>
