47b0f12c504c6843e6ba849cacc37400bacf506e
bailobarry <barrymamadoubailo23@gmail.com>
2024-03-16 17:06:30 +0100

    Ajout composant pour front-end

---
+++ src/Controller/admin/UtilisateursController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UtilisateursController extends AbstractController
+{
+    //#[Route('/utilisateurs', name: 'app_utilisateurs')]
+    /*public function index(): Response
+    {
+        return $this->render('utilisateurs/index.html.twig', [
+            'controller_name' => 'UtilisateursController',
+        ]);
+    }*/
+}
